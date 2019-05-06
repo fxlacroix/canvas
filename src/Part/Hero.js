4c8e53e868cce8d65bbe86f00a2e8a4fdb45966a
François-Xavier LACROIX <fxlacroix@gmail.com>
2019-05-06 16:11:49 +0200

    no more idea

---
+++ src/Part/Hero.js
@@ -0,0 +1,16 @@
+/**
+ * Hero
+ */
+class Hero {
+
+    constructor(){
+        // canvas
+        this.canvas = document.querySelector('canvas')
+        // context
+        this.c = this.canvas.getContext('2d')
+    }
+
+
+}
+
+module.exports = Hero
