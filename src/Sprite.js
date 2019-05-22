c53f5828109d6f0f32737468c302eaf75a90fe55
François-Xavier LACROIX <fxlacroix@gmail.com>
2019-05-22 14:58:13 +0200

    simplify

--- src/Sprite.js
+++ src/Sprite.js
@@ -68,67 +68,22 @@ class Sprite extends Canvas {
     listenKeys(grid, keys){
 
         // UP
-        if (keys[this.keyUp] ||
-            (this.stillMoving[this.keyUp] && this.stillMoving[this.keyUp].length)) {
-
-            if(this.stillMoving[this.keyUp] && this.stillMoving[this.keyUp].length){
-                let mvmt = this.stillMoving[this.keyUp].shift()
-                if(! this.stillMoving[this.keyUp].length) {
-                    keys[this.keyUp] = false;
-                }
-            } else {
-                this.stillMoving[this.keyUp] = [1, 1, 1, 1, 1]
-            }
-
+        if (keys[this.keyUp]) {
             this.update(grid,0, -this.movementSpeed, this.facingUp)
-
         }
 
         // DOWN
-        if (keys[this.keyDown] ||
-            (this.stillMoving[this.keyDown] && this.stillMoving[this.keyDown].length)) {
-
-            if(this.stillMoving[this.keyDown] && this.stillMoving[this.keyDown].length){
-                let mvmt = this.stillMoving[this.keyDown].shift()
-                if(! this.stillMoving[this.keyDown].length) {
-                    keys[this.keyDown] = false;
-                }
-            } else {
-                this.stillMoving[this.keyDown] = [1, 1, 1, 1, 1]
-            }
-
+        if (keys[this.keyDown]) {
             this.update(grid,0, this.movementSpeed, this.facingDown)
         }
 
         // LEFT
-        if (keys[this.keyLeft] ||
-            (this.stillMoving[this.keyLeft] && this.stillMoving[this.keyLeft].length)) {
-            if(this.stillMoving[this.keyLeft]  && this.stillMoving[this.keyLeft].length){
-                let mvmt = this.stillMoving[this.keyLeft].shift()
-                if(! this.stillMoving[this.keyLeft].length) {
-                    keys[this.keyLeft] = false;
-                }
-            } else {
-                this.stillMoving[this.keyLeft] = [1, 1, 1, 1, 1]
-            }
-
+        if (keys[this.keyLeft]) {
             this.update(grid, -this.movementSpeed, 0, this.facingLeft)
-
         }
 
         // KEYRIGHT
-        if (keys[this.keyRight] ||
-            (this.stillMoving[this.keyRight] && this.stillMoving[this.keyRight].keyRight)) {
-
-            if(this.stillMoving[this.keyRight] && this.stillMoving[this.keyRight].keyRight){
-                let mvmt = this.stillMoving[this.keyRight].shift()
-                if(! this.stillMoving[this.keyRight].length) {
-                    keys[this.keyRight] = false;
-                }
-            } else {
-                this.stillMoving[this.keyRight] = [1, 1, 1, 1, 1]
-            }
-
+        if (keys[this.keyRight]) {
             this.update(grid, this.movementSpeed, 0, this.facingRight)
         }
     }
