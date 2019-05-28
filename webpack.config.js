82b65899c221da73f2568a2a55d2f5be8e31cf30
François-Xavier Lacroix <fxlacroix@gmail.com>
2019-05-28 11:21:57 +0000

    serving images

--- webpack.config.js
+++ webpack.config.js
@@ -2,44 +2,54 @@ const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  mode: 'development',
-  entry: './src/Game.js',
-  output: {
-    path: __dirname + '/dist/',
-    filename: './js/Game.bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        //exclude: /(node_modules|bower_components)/,
-        exclude: /node_modules(?!\/my-es6-raw-module)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['env']
-          }
-        }
-      },
-      {
-        test: /\.css$/,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
-      },
-    ]
-  },
-  plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['dist'] },
-      files: ['./dist/*'],
-      notify: false
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/Template/index.html'
-    })
-  ],
-  watch: true,
-  devtool: 'source-map'
+    mode: 'development',
+    entry: './src/Game.js',
+    output: {
+        path: __dirname + '/dist/',
+        filename: './js/Game.bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                //exclude: /(node_modules|bower_components)/,
+                exclude: /node_modules(?!\/my-es6-raw-module)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [{loader: 'style-loader'}, {loader: 'css-loader'}],
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8000, // Convert images < 8kb to base64 strings
+                        name: 'images/[hash]-[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            server: {baseDir: ['dist']},
+            files: ['./dist/*'],
+            notify: false
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/Template/index.html'
+        })
+    ],
+    watch: true,
+    devtool: 'source-map'
 }
